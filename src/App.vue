08fabd8e951bdbf76da75510a5ce46d70f6bf37b
GilangHyrs <gilanghadyr@gmail.com>
2024-05-24 23:58:19 +0700

    Menambahkan Delete

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@
           swipeable
           transition-prev="slide-right"
           transition-next="slide-left"
-          height="550px"
+          height="1000px"
           class="bg-dark text-white shadow-2 rounded-borders"
         >
           <q-carousel-slide v-for="(image, index) in carouselImages" :key="index" :name="index" :img-src="image">
@@ -54,9 +54,9 @@
 
         <!-- Cards -->
         <div class="q-mt-xl row justify-center">
-          <q-card v-for="product in products" :key="product.name" class="my-card q-mb-xl q-pa-md col-xs-12 col-sm-6 col-md-4 col-lg-3">
+          <q-card v-for="(product, index) in products" :key="product.name" class="my-card q-mb-xl q-pa-md col-xs-12 col-sm-6 col-md-4 col-lg-3">
             <q-img :src="product.image" :alt="product.name" class="my-card-img">
-              <q-badge color="red" floating>{{ product.discount }}Flash Sale</q-badge>
+              <q-badge color="red" floating>{{ product.discount }} Flash Sale</q-badge>
             </q-img>
             <q-card-section>
               <div class="text-h6 text-center">{{ product.name }}</div>
@@ -64,6 +64,7 @@
             </q-card-section>
             <q-card-actions align="center">
               <q-btn flat label="Add to Cart" color="primary" @click="addToCart(product)" />
+              <q-btn flat label="Delete" color="negative" @click="deleteProduct(index)" />
             </q-card-actions>
           </q-card>
         </div>
@@ -94,17 +95,20 @@ export default {
     const leftDrawerOpen = ref(false);
     const slide = ref(0);
     const carouselImages = [
-      'https://th.bing.com/th/id/OIP.4Xys25CzrknwT15IQzz0dAHaHa?w=207&h=207&c=7&r=0&o=5&dpr=1.3&pid=1.7',  // Ganti dengan URL gambar kita
-      'https://th.bing.com/th/id/OIP.g6j4DVM6W0hE4B7MYxRZLwHaGR?w=220&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7',
-      'https://cdn1.productnation.co/stg/sites/5/5c99ab9c32066.jpeg'
+      'https://th.bing.com/th/id/OIP.2h5a2vDMHJIhMf8iFLst2QHaH0?w=164&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+      'https://th.bing.com/th/id/OIP.GYW9Y5A8YpW8gHpUzVfjSQHaE8?w=305&h=204&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+      'https://th.bing.com/th/id/OIP.IJyES2cc-sZA88cRNCTVKwHaHa?w=199&h=199&c=7&r=0&o=5&dpr=1.3&pid=1.7'
     ];
-    const products = [
-      { name: 'Baju Kaos  1', price: 'Rp. 100K', image: 'https://th.bing.com/th/id/OIP.0J-HAj7uzUfE-_dVV_reBQAAAA?w=199&h=199&c=7&r=0&o=5&dpr=1.3&pid=1.7'},  // Ganti dengan URL gambar kita
-      { name: 'Baju Kaos 2', price: 'Rp. 200K', image: 'https://th.bing.com/th/id/OIP.Fndm_SktZ9bIAjj4cmEhVgHaHa?w=185&h=186&c=7&r=0&o=5&dpr=1.3&pid=1.7'},
-      { name: 'Baju Kaos 3', price: 'Rp. 250K', image: 'https://th.bing.com/th/id/OIP.j7UlAXVytwTtfjuv_69vZgHaHa?w=163&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'},
-      { name: 'Diamond', price: 'Rp. 20k-250K', image: 'https://th.bing.com/th/id/OIP.Svz9PVP96vvqcGzdhoZipQHaHc?w=143&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'},
-      { name: 'Baju  Kaos 5', price: 'Rp. 300K', image: 'https://th.bing.com/th/id/OIP.ADoNtmgonhpLsrC3pkY-wgHaHa?w=206&h=206&c=7&r=0&o=5&dpr=1.3&pid=1.7'}
-    ];
+    const products = ref([
+      { name: 'Baju Kaos 3 second', price: 'Rp. 100K', image: 'https://th.bing.com/th/id/OIP.0J-HAj7uzUfE-_dVV_reBQAAAA?w=199&h=199&c=7&r=0&o=5&dpr=1.3&pid=1.7', discount: '20%' },
+      { name: 'Baju Kaos 3 second', price: 'Rp. 200K', image: 'https://th.bing.com/th/id/OIP.Fndm_SktZ9bIAjj4cmEhVgHaHa?w=185&h=186&c=7&r=0&o=5&dpr=1.3&pid=1.7', discount: '15%' },
+      { name: 'Baju Kaos 3 second', price: 'Rp. 250K', image: 'https://th.bing.com/th/id/OIP.j7UlAXVytwTtfjuv_69vZgHaHa?w=163&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7', discount: '10%' },
+      { name: 'Diamond Mobile legend ', price: 'Rp. 20k-250K', image: 'https://th.bing.com/th/id/OIP.Svz9PVP96vvqcGzdhoZipQHaHc?w=143&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7', discount: '5%' },
+      { name: 'Tas Ransel', price: 'Rp. 250K', image: 'https://th.bing.com/th?id=OIP.QlFa77m4cd7UJ0oJfIP7EgHaI4&w=228&h=273&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2', discount: '10%' },
+      { name: 'UC PUBG', price: 'Rp. 15k-250K', image: 'https://th.bing.com/th/id/OIP.oJo4dbCSmycD1sO8JCux8wHaHa?w=176&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7', discount: '10%' },
+      { name: 'Kemeja Hitam', price: 'Rp. 250K', image: 'http://ts1.mm.bing.net/th?id=OIP.C3PAS5cligqapH5JVJ8xSQHaHa&pid=15.1', discount: '10%' },
+      { name: 'Baju Kaos 3 second', price: 'Rp. 300K', image: 'https://th.bing.com/th/id/OIP.ADoNtmgonhpLsrC3pkY-wgHaHa?w=206&h=206&c=7&r=0&o=5&dpr=1.3&pid=1.7', discount: '25%' }
+    ]);
     const cart = ref([]);
 
     const toggleLeftDrawer = () => {
@@ -121,6 +125,11 @@ export default {
       console.log(`${product.name} telah ditambahkan ke keranjang`);
     };
 
+    const deleteProduct = (index) => {
+      products.value.splice(index, 1);
+      console.log(`Product at index ${index} has been deleted`);
+    };
+
     return {
       leftDrawerOpen,
       toggleLeftDrawer,
@@ -128,7 +137,8 @@ export default {
       carouselImages,
       products,
       addToCart,
-      goToCart
+      goToCart,
+      deleteProduct
     };
   }
 };
@@ -142,13 +152,16 @@ export default {
   border-radius: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 }
+
 .my-card-img {
   border-radius: 10px 10px 0 0;
   height: 200px;
 }
+
 .rounded-borders {
   border-radius: 10px;
 }
+
 .q-footer {
   border-top: 1px solid #7a0085;
 }
--- src/Router/index.js
+++ src/Router/index.js
@@ -1,11 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-import HomeView from '../pages/HomeView.vue'
-import AboutView from '../pages/AboutView.vue'
+import HomeView from '../views/Home.vue' // Adjust the import path
+import ShoppingCartView from '../views/ShoppingCart.vue' // Adjust the import path
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/about', component: AboutView },
+  { path: '/about', component: ShoppingCartView }, // Assuming '/about' corresponds to ShoppingCart
 ]
 
 const router = createRouter({
